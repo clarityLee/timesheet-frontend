2a0ad181f0092943ffe71eb1df500a37ea3c1727
Yunjing Lee <clarity.lee@gmail.com>
2021-11-04 09:16:50 -0400

    Router and NavBar

---
+++ src/components/profile/Profile.js
@@ -0,0 +1,15 @@
+import React from "react";
+import NavBar from "../navbar/NavBar";
+
+const Profile = () => {
+  return (
+    <div>
+      <NavBar />
+      <div>
+        Profile
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
---
+++ src/components/timesheet/TimeSheet.js
@@ -0,0 +1,15 @@
+import React from "react";
+import NavBar from "../navbar/NavBar";
+
+const TimeSheet = () => {
+  return (
+    <div>
+      <NavBar />
+      <div>
+        Time Sheet
+      </div>
+    </div>
+  );
+};
+
+export default TimeSheet;
