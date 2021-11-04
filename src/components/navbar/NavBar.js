78f7bb96f796432e8774fb62dbb3fe1b83041f11
Xian Zhang <xianzhang9825@gmail.com>
2021-11-04 15:57:03 -0400

    wip, summary page

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,14 +1,13 @@
 import React, { useState } from "react";
-import {Link} from "react-router-dom";
-import { useNavigate  } from 'react-router-dom';
-import './NavBar.css';
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import "./NavBar.css";
 
 const NavBar = () => {
+  const [isAutheticated, setisAutheticated] = useState(false);
 
-  const[ isAutheticated, setisAutheticated ] = useState(false);
-
-  let navigate = useNavigate ();
-  let handleLogout = e => {
+  let navigate = useNavigate();
+  let handleLogout = (e) => {
     e.preventDefault();
     setisAutheticated(false);
     window.sessionStorage.removeItem("username");
@@ -16,10 +15,18 @@ const NavBar = () => {
   };
   return (
     <div>
-      <Link to="/summary" className="navItem">Summary</Link>
-      <Link to="/timesheet" className="navItem">TimeSheet</Link>
-      <Link to="/profile" className="navItem">Profile</Link>
-      <button className="navItem" onClick={handleLogout}>Logout</button>
+      <Link to="/summary" className="navItem">
+        Summary
+      </Link>
+      <Link to="/timesheet" className="navItem">
+        TimeSheet
+      </Link>
+      <Link to="/profile" className="navItem">
+        Profile
+      </Link>
+      <button className="navItem" onClick={handleLogout}>
+        Logout
+      </button>
     </div>
   );
 };
